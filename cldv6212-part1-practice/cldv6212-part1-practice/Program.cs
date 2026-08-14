1020dc2827634edcc4a3a1071e576e47aab49e2e
ST10469982-Tarun-Soodyall <ST10469982@myemeris.edu.za>
2026-08-14 10:45:59 +0200

    pushing

---
+++ cldv6212-part1-practice/cldv6212-part1-practice/Program.cs
@@ -0,0 +1,9 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello world");
+        Console.WriteLine("This is a c# console application");
+        Console.WriteLine("Running on .NET 10.0 with c# 14.0 features");
+    }
+}
